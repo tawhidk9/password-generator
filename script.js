94517edfaa32913c7b26af2873b7142543a3e575
Tawhid <tawhidkamal5@gmail.com>
2023-05-18 22:47:52 -0400

    added random generator and writePassword function

--- script.js
+++ script.js
@@ -15,6 +15,59 @@ var userInt = parseInt(prompt("Please input a password length (at least 8 charac
 // console log user input
 console.log(userInt)
 
+//"if" condition for inputs
+if (!userInt) {
+  alert("Please input a numeric value.")
+  return generatePassword()
+} else if (userInt < 8 || userInt > 128) {
+  alert("Please input a value between 8 & 128.")
+  return generatePassword()
+} else {
+  console.log('conditions met')
+
+  var confirmLower = confirm("Include Lower Case?");
+  var confirmUpper = confirm("Include Upper Case?");
+  var confirmNums = confirm("Include Numbers?");
+  var confirmSyms = confirm("Include Symbols?");
+}
+
+//array concat
+
+if(confirmNums) {
+  resArray = resArray.concat(numS);
+}
+if(confirmLower) {
+  resArray = resArray.concat(lowChar);
+}
+if(confirmUpper) {
+  resArray = resArray.concat(upChar);
+}
+if(confirmSyms) {
+  resArray = resArray.concat(symS);
+}
+
+// logged resArray to see if it concatted correctly
+console.log(resArray)
+
+// random generator
+  
+for (var i = 0; i < userInt; i++) {
+  // pushes our resArray into userArray
+  userArray.push(resArray[Math.floor(Math.random() * resArray.length)]);
+}
+
+console.log(userArray)
+
+// converts returned userArray into string
+return userArray.join("");
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  // password.Text will first run gen func then paste output val into passwordText area
+  passwordText.value = password;
 
 }
 
