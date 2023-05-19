3c500dd78a4f26587d1cc5ea394370522bea9fa2
Tawhid <tawhidkamal5@gmail.com>
2023-05-18 22:40:52 -0400

    added if statement for inputs

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-  // Arrays
+// Arrays
 
 var upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symS = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+// prompt 
+var userInt = parseInt(prompt("Please input a password length (at least 8 characters but no more than 128 characters)."));
+
+// console log user input
+console.log(userInt)
+
+
 }
 
 // Add event listener to generate button
