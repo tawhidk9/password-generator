a082f7a7e77e4b154bcd6d768fd00ee6491e451b
Tawhid <tawhidkamal5@gmail.com>
2023-05-18 22:38:05 -0400

    added array with all valid values

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword(){
+  // Arrays
 
-  passwordText.value = password;
-
+var upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symS = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 }
 
 // Add event listener to generate button
